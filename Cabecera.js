1cd12cd92d5744493f7fec5a0bc84cad085d4a31
Sofía Suárez Fernández <60401906+uo270149@users.noreply.github.com>
2020-12-01 19:45:19 +0100

    Add files via upload
    Ejercicio 1

---
+++ Cabecera.js
@@ -0,0 +1,8 @@
+var asignatura = new Object();
+asignatura.nombre = "Software y estÃ¡ndares en la Web";
+asignatura.titulacion = "Grado en IngenierÃ­a InformÃ¡tica del Software";
+asignatura.centro = "Escuela de IngenierÃ­a InformÃ¡tica";
+asignatura.universidad = "Universidad de Oviedo";
+asignatura.curso = "2017-18";
+asignatura.profesor = "Juan Manuel Cueva Lovelle";
+asignatura.email = "cueva@uniovi.es";
