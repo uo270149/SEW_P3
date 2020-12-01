edcfabf15221d6920bb55f2c0f07adcdf2c01f6a
Sofía Suárez Fernández <60401906+uo270149@users.noreply.github.com>
2020-12-01 22:40:06 +0100

    Add files via upload
    ej 11.1

---
+++ Ejercicio11-1.js
@@ -0,0 +1,42 @@
+"use strict";
+class GeoLocalizacion {
+    constructor() {
+        navigator.geolocation.getCurrentPosition(this.getPosicion.bind(this));
+    }
+
+    getPosicion(posicion) {
+        this.longitud = posicion.coords.longitude;
+        this.latitud = posicion.coords.latitude;
+        this.precision = posicion.coords.accuracy;
+        this.altitud = posicion.coords.altitude;
+        this.precisionAltitud = posicion.coords.altitudeAccuracy;
+        this.rumbo = posicion.coords.heading;
+        this.velocidad = posicion.coords.speed;
+    }
+
+    getLongitud() {
+        return this.longitud;
+    }
+    getLatitud() {
+        return this.latitud;
+    }
+    getAltitud() {
+        return this.altitud;
+    }
+    mostrar(donde) {
+        var ubicacion = document.getElementById(donde);
+        var datos = "<ul>";
+
+        datos += "<li>Longitud: " + this.longitud + " grados</li>";
+        datos += "<li>Latitud: " + this.latitud + " grados</li>";
+        datos += "<li>Precisión de la latitud y la longitud: " + this.precision + " metros</li>";
+        datos += "<li>Altitud: " + this.altitud + " metros</li>";
+        datos += "<li>Precisión de la altitud: " + this.precisionAltitud + " metros</li>";
+        datos += "<li>Rumbo: " + this.rumbo + " grados</li>";
+        datos += "<li>Velocidad: " + this.velocidad + " m/s</li>";
+        datos += "</ul>"
+
+        ubicacion.innerHTML = datos;
+    }
+}
+var geo = new GeoLocalizacion();
