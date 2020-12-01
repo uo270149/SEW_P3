87bde77780a0d770ac5a51c63760fb7405a46249
Sofía Suárez Fernández <60401906+uo270149@users.noreply.github.com>
2020-12-01 20:40:38 +0100

    Add files via upload

--- CalculadoraBasica.js
+++ CalculadoraBasica.js
@@ -1,34 +1,75 @@
 "use strict";
-class Calculator{
-    constructor(){
-        this.stack=0;
+class Calculadora {
+    constructor() {
+        this.operation = "";
+        this.showedOperation = "";
+        this.memory = "";
+        this.solved = false;
     }
-    calculate(){
-        try{
-        document.getElementById('screen').value = eval(document.getElementById('screen').value);
-        }catch(err){ 
-        document.getElementById('screen').value = "ERROR";
+    refrescar(){
+        document.getElementById("resultado").textContent = this.showedOperation;
+    }
+    digitos(numero) {
+        if(this.solved) {
+            document.getElementById("resultado").textContent = "";
+            this.solved = false;
+        } 
+        this.operation += numero;
+        this.showedOperation += numero;
+        this.refrescar();
+    }
+    punto() {
+        this.operation += ".";
+        this.showedOperation += ".";
+        this.refrescar();
+    }
+    suma() {
+        this.operation += "+";
+        this.showedOperation += "+";
+        this.refrescar();
+    }
+    resta() {
+        this.operation += "-";
+        this.showedOperation += "-";
+        this.refrescar();
+    }
+    multiplicacion() {
+        this.operation += "*";
+        this.showedOperation += "*";
+        this.refrescar();
+    }
+    division() {
+        this.operation += "/";
+        this.showedOperation += "/";
+        this.refrescar();
+    }
+    mrc() {
+        var result = this.memory;
+        document.getElementById("resultado").textContent = result;
+        this.operation = result;
+        this.showedOperation = result;
+        this.memory = "";
+    }
+    mMenos() {
+        if(!isNaN(document.getElementById("resultado").textContent)){
+            this.memory = eval(this.memory + "-" + document.getElementById("resultado").textContent);
         }
     }
-    show(digit){
-        var last = document.getElementById('screen').value;
-        document.getElementById('screen').value = last.concat(digit);
+    mMas() {
+        if(!isNaN(document.getElementById("resultado").textContent)){
+            this.memory = eval(this.memory + "+" + document.getElementById("resultado").textContent);
+        }
     }
-    clearScreen(){
-        document.getElementById('screen').value = null;
-    } 
-    
-    showLast(){
-        document.getElementById('screen').value = this.stack;
+    borrar() {
+        document.getElementById("resultado").textContent = "";
+        this.operation = "";
+        this.showedOperation = "";
     }
-    
-    mAdd(){
-        this.stack += parseInt(document.getElementById('screen').value);
-    }
-    mSub(){
-        this.stack -= parseInt(document.getElementById('screen').value);
+    igual() {
+        this.operation = eval(this.operation);
+        this.showedOperation = eval(this.operation);
+        this.refrescar();
+        this.solved = false;
     }
 }
-
-
-var calculator = new Calculator();
+var calculadora = new Calculadora();
